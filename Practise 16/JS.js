7cd3677f4954c6c4acfe05c323af3bbb1a83b7ad
XxLycorisxX <ap.khomyakov@gmail.com>
2025-08-15 15:56:11 +0300

    Adding HW 13 (MAKET_ABS) + HW 14 (Work in Progress)

---
+++ Practise 16/JS.js
@@ -0,0 +1,25 @@
+"use strict";
+let UserName = "Иван";
+let UserAge = 25;
+UserAge = 26;
+let Phrase = `Привет, я ${UserName} и мне ${UserAge} лет`;
+console.log(Phrase);
+
+let a = (50 - 15) * 7 + 18 / 3;
+let b = (4 ** 4 + 5 ** 3) / 3 ** 2;
+let c = (115 % 75) / 2;
+console.log(a);
+console.log(b);
+console.log(c);
+
+let second = 24;
+second -= 6;
+second += 11;
+second -= 8;
+second += 9;
+console.log(second);
+
+let ton = 3;
+ton /= 20;
+ton *= 1000;
+console.log(ton);
