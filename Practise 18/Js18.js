30cae8c51234f2f7a4e4614255711f7f13ca993f
XxLycorisxX <ap.khomyakov@gmail.com>
2025-08-23 14:23:25 +0300

    Adding HW 17 (JS ANKETA)

---
+++ Practise 18/Js18.js
@@ -0,0 +1,49 @@
+//Task 1
+// let a = Number(prompt("Введите натуральное число"));
+// let i = 1;
+// while (i < a) {
+//   console.log(i);
+//   i++;
+// }
+
+//Task 2
+// let a = 80;
+// let i = -10;
+
+// while (i <= a) {
+//   console.log(i);
+//   i += 10;
+// }
+
+//Task 3
+// let i = 0;
+// while (i > -20) {
+//   if (i % 2 != 0) {
+//     console.log;
+//   }
+//   i--;
+// }
+
+//Task 4
+// let b = 3;
+// let a = null;
+// do {
+//   a = Number(prompt("Угадайте число от 1 до 20"));
+// } while (!a || a != b);
+// console.log(a);
+// if ((a = b)) {
+//   alert("Угадали!");
+// } else {
+//   alert("Неугдали!");
+// }
+
+//Task 5
+// for (let i = -10; i <= 80; i += 10) {
+//   console.log(i);
+// }
+
+//Task 6
+// for (let i = 1; i <= 19; i++) {
+//   if (i % 2 == 0 || i == 13) continue;
+//   console.log(i);
+// }
